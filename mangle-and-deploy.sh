aad1bf9eff0f2fba937b4dbc0f4a130884942778
Dan Wolfe <dwolfe2@nd.edu>
2017-07-27 14:54:59 -0400

    don't replace that

--- mangle-and-deploy.sh
+++ mangle-and-deploy.sh
@@ -3,7 +3,7 @@ BUCKET=resources.library.nd.edu
 
 function doStuff {
   # mangle html
-  munch --css ./template --html ./template --ignore .nd,.body,.classList,.library,.readyState,.status
+  munch --css ./template --html ./template --ignore .nd,.body,.classList,.library,.readyState,.status,.head
   # upload to aws
   aws s3 sync ./template_opt s3://${BUCKET}/frame --exclude '.*' --exclude '*.md' --acl public-read --profile wseAdmin
   echo "\033[0;31mDeployed to ${BUCKET}.s3-website-us-east-1.amazonaws.com/frame."
