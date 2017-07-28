78bfaeafb9c817b42827af092ad7cd3939953134
Dan Wolfe <dwolfe2@nd.edu>
2017-07-28 09:42:27 -0400

    clean up echoes

--- mangle-and-deploy.sh
+++ mangle-and-deploy.sh
@@ -6,14 +6,14 @@ function doStuff {
   munch --css ./template --html ./template --ignore .nd,.body,.classList,.library,.readyState,.status,.head
   # upload to aws
   aws s3 sync ./template_opt s3://${BUCKET}/frame --exclude '.*' --exclude '*.md' --acl public-read --profile wseAdmin
-  echo "\033[0;31mDeployed to ${BUCKET}.s3-website-us-east-1.amazonaws.com/frame."
+  echo "Deployed to ${BUCKET}.s3-website-us-east-1.amazonaws.com/frame."
 }
 
 function installStuff {
   pushd html-muncher
   python setup.py install
   popd
-    echo "\033[0;31mMunch was not installed, so we installed it for you."
+    echo "Munch was not installed, so we installed it for you."
 }
 
 # check if munch is installed
