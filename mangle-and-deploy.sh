7b077e88471c0e4dc1bf87b73220cc436ed98c24
Justin Gondron <jgondron@nd.edu>
2017-09-14 14:00:54 -0400

    - No idea what this is going to break, but adding .b to the ignore seems to be the only way to get the hours.b.png to show up.

--- mangle-and-deploy.sh
+++ mangle-and-deploy.sh
@@ -3,7 +3,7 @@ BUCKET=resources.library.nd.edu
 
 function doStuff {
   # mangle html
-  munch --css ./template --html ./template --html ./examples --ignore .nd,.body,.classList,.library,.readyState,.status,.head,.s3-website-us-east-1,.amazonaws,.com,.edu,.href,.title
+  munch --css ./template --html ./template --html ./examples --ignore .nd,.body,.classList,.library,.readyState,.status,.head,.s3-website-us-east-1,.amazonaws,.com,.edu,.href,.title,.b
   # upload to aws
   aws s3 sync ./template_opt s3://${BUCKET}/frame --exclude '.*' --exclude '*.md' --acl public-read --profile wseAdmin
   echo "Deployed to ${BUCKET}.s3-website-us-east-1.amazonaws.com/frame."
