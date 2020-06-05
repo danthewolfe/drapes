4c99d90ce0c33fef90439d16c6b59778fa7354de
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-06-05 10:46:22 -0400

    Boy this muncher thing is garbage...

--- mangle-and-deploy.sh
+++ mangle-and-deploy.sh
@@ -3,7 +3,7 @@ BUCKET=resources.library.nd.edu
 
 function doStuff {
   # mangle html
-  munch --css ./template --html ./template --html ./examples --ignore .nd,.body,.classList,.library,.readyState,.status,.head,.s3-website-us-east-1,.amazonaws,.com,.edu,.href,.title,.b,.alerts,.alert-section,.alert-container,.alert-description,.informational,.informational-yellow,.warning,.execute-api,.us-east-1
+  munch --css ./template --html ./template --html ./examples --ignore .nd,.body,.classList,.library,.readyState,.status,.head,.s3-website-us-east-1,.amazonaws,.com,.edu,.href,.title,.b,.alerts,.alert-section,.alert-container,.alert-description,.informational,.informational-yellow,.warning,.execute-api,.us-east-1,.responseText,.onload,.onerror
   # munch screws up images so let's get rid of that, so we don't accidentally upload it
   rm -R ./template_opt/images
   # upload to aws
